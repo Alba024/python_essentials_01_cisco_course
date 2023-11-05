a507ad97a549917b3b17fb4aabd7cda5158f8273
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2023-11-05 21:34:46 +0100

    Project Init

---
+++ TaxThalers.py
@@ -0,0 +1,13 @@
+# Excercise 3.1.11 LAB 
+
+income = float(input("Enter the annual income: "))  
+
+if income < 85528:
+    tax = income * 0.18 - 556.02
+elif income > 85528 :
+    tax = 14839.02 + ((income - 85528) * 0.32) 
+    
+tax = round(tax, 0)
+
+print("Your tax is: ", tax, "thalers")        
+    
---
+++ spathiphyllum.py
@@ -0,0 +1,10 @@
+# Excercise 3.1.10 cisco course
+plant_name = str(input("Enter the name of the plant:"))
+
+if plant_name == "Spathiphyllum" :
+    print("Spathiphyllum is the best plant ever!")
+elif plant_name == "spathiphyllum" :
+    print(" No, I want a big Spathiphyllum!")
+else :
+    print("Spathiphyllum! Not " + plant_name + "!")
+    
