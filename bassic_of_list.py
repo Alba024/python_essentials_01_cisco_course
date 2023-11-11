46a687011ef44452567b0b2bda9d52ea9a5562fb
Alba de Rosales <derosalesalba@gmail.com>
2023-11-11 13:24:49 +0100

    Lists_on_py

---
+++ bassic_of_list.py
@@ -0,0 +1,10 @@
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers
+
+user_number = input("Enter a number: ") # Step 1: write a line of code that prompts the user
+hat_list[2] = user_number # to replace the middle number with an integer number entered by the user.
+
+del hat_list[4] # Step 2: write a line of code that removes the last element from the list.
+print("Lenght of the existing list", len(hat_list)) # Step 2: write a line of code 
+# that removes the last element from the list.
+
+print(hat_list)
